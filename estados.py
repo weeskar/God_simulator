d3955e709d6f6e9a4c02c500ff371e2d55bf80d2
weeskar <142720468+weeskar@users.noreply.github.com>
2023-10-28 16:54:09 +0200

    Delete estados.py

--- estados.py
+++
@@ -1,9 +0,0 @@
-#estados.py
-from enum import Enum
-
-class EstadosJuego(Enum):
-    MENU_INICIO = "menu_inicio"
-    OPCIONES_MENU_INICIO = "opciones_menu_inicio"
-    JUGANDO = "jugando"
-    SALIR = "salir"
-    MENU_OPCIONES_JUGANDO = "menu_opciones_jugando"  # Agrega esta línea
